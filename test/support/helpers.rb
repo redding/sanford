56915478b3e05c4ee0465205b09956c55e7bdb9b
jcredding <TempestTTU@gmail.com>
2013-02-27 12:27:59 -0600

    Remove managing tests for now, issues with forked processes
    These tests are not properly handling the processes they spawn
    which causes processes to be left running. This can cause lots of
    problems when running the test suite multiple times in a row.
    For now, I'd rather remove these tests, and re-think how to
    properly test this logic. The unit tests should provide decent
    coverage, and the bench host can always be started to tests the
    full stack.

--- test/support/helpers.rb
+++ test/support/helpers.rb
@@ -1,6 +1,6 @@
 module Test
 
-  module ForkServerHelper
+  module SpawnServerHelper
 
     def start_server(host, &block)
       begin
@@ -12,32 +12,6 @@ module Test
         server.halt if server
         thread.join if thread
       end
-    end
-
-  end
-
-  module ManagerHelper
-
-    # start a Sanford server using Sanford's manager in a forked process
-    def fork_and_call(proc, &block)
-      pid = fork do
-        STDOUT.reopen('/dev/null') unless ENV['SANFORD_DEBUG']
-        manager = proc.call
-        trap("TERM"){ manager.stop }
-      end
-      sleep 0.3 # give time for the command to run
-      yield
-    ensure
-      if pid
-        Process.kill("TERM", pid)
-        Process.wait(pid)
-      end
-    end
-
-    def open_socket(host, port)
-      socket = TCPSocket.new(host, port)
-    ensure
-      socket.close rescue false
     end
 
   end
