4b115d198a6997404e23f72da16e17bd3a0d4fe6
Kelly Redding <kelly@kellyredding.com>
2013-03-16 12:15:03 -0500

    updates pid file tests to not assume running out of the root dir
    These tests were breaking if you didn't run them from the root dir.

--- test/unit/manager_pid_file_test.rb
+++ test/unit/manager_pid_file_test.rb
@@ -6,24 +6,25 @@ class Sanford::Manager::PIDFile
   class BaseTest < Assert::Context
     desc "Sanford::Manager::PIDFile"
     setup do
-      @pid_file = Sanford::Manager::PIDFile.new("tmp/my.pid")
+      @pid_file_path = File.join(ROOT, "tmp/my.pid")
+      @pid_file = Sanford::Manager::PIDFile.new(@pid_file_path)
     end
     teardown do
-      FileUtils.rm_rf("tmp/my.pid")
+      FileUtils.rm_rf(@pid_file_path)
     end
     subject{ @pid_file }
 
     should have_instance_methods :pid, :to_s, :write, :remove
 
     should "return it's path with #to_s" do
-      assert_equal "tmp/my.pid", subject.to_s
+      assert_equal @pid_file_path, subject.to_s
     end
 
     should "write the pid file with #write" do
       subject.write
 
-      assert File.exists?("tmp/my.pid")
-      assert_equal "#{Process.pid}\n", File.read("tmp/my.pid")
+      assert_file_exists @pid_file_path
+      assert_equal "#{Process.pid}\n", File.read(@pid_file_path)
     end
 
     should "return the value stored in the pid value with #pid" do
@@ -36,7 +37,7 @@ class Sanford::Manager::PIDFile
       subject.write
       subject.remove
 
-      assert_not File.exists?("tmp/my.pid")
+      assert_not File.exists?(@pid_file_path)
     end
 
   end
