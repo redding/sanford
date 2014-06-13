559c2fa4812855a63f59c80e0aefe431107950ed
Collin Redding <TempestTTU@gmail.com>
2014-06-13 15:39:25 -0500

    Add `Router` and `Route` for routing and running services
    This adds a `Router` and `Route` class. The `Router` will replace
    part of the `Host` and `HostData` logic for routing services. This
    also adds a `Route` class that will be built when a service is
    added to the router. This will handler building and running a
    `SanfordRunner` and returning the response. This follows Deas'
    implementation and is part of the effort to modify Sanford's
    configuration and startup logic.

---
+++ test/unit/router_tests.rb
@@ -0,0 +1,65 @@
+require 'assert'
+require 'sanford/router'
+
+require 'test/support/factory'
+
+class Sanford::Router
+
+  class UnitTests < Assert::Context
+    desc "Sanford::Host"
+    setup do
+      @router = Sanford::Router.new
+    end
+    subject{ @router }
+
+    should have_readers :routes
+    should have_imeths :service_handler_ns, :service
+
+    should "build an empty array for its routes by default" do
+      assert_equal [], subject.routes
+    end
+
+    should "not have a service handler ns by default" do
+      assert_nil subject.service_handler_ns
+    end
+
+    should "allow setting its service handler ns" do
+      namespace = Factory.string
+      subject.service_handler_ns namespace
+      assert_equal namespace, subject.service_handler_ns
+    end
+
+    should "allow adding routes using `service`" do
+      service_name = Factory.string
+      handler_name = Factory.string
+      subject.service service_name, handler_name
+
+      route = subject.routes.last
+      assert_instance_of Sanford::Route, route
+      assert_equal service_name, route.name
+      assert_equal handler_name, route.handler_class_name
+    end
+
+    should "use its service handler ns when adding routes" do
+      namespace = Factory.string
+      subject.service_handler_ns namespace
+
+      service_name = Factory.string
+      handler_name = Factory.string
+      subject.service service_name, handler_name
+
+      route = subject.routes.last
+      expected = "#{namespace}::#{handler_name}"
+      assert_equal expected, route.handler_class_name
+    end
+
+    should "know its custom inspect" do
+      reference = '0x0%x' % (subject.object_id << 1)
+      expected = "#<#{subject.class}:#{reference} " \
+                   "@service_handler_ns=#{subject.service_handler_ns.inspect}>"
+      assert_equal expected, subject.inspect
+    end
+
+  end
+
+end
