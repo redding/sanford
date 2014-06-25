9319e97d2cd381eac7ad322022a2b738f9775986
Collin Redding <TempestTTU@gmail.com>
2014-06-25 14:03:48 -0500

    Stop using `Rake` for running benchmarks
    This changes the benchmarks to stop using `Rake` and just use
    the Sanford CLI and ruby scripts. This minimizes using Rake with
    the gem and only requires it for gem building. I'd rather not rely
    on extra dependencies that are not needed and for this case, Rake
    isn't needed.
    
    This removes the "run X requests against the Benchmark server"
    functionality. This hasn't been used since Sanford has been stable
    and isn't related to "benchmarking". It was more of a system test
    to see that everything worked when running a server.

--- bench/client.rb
+++ bench/client.rb
@@ -10,9 +10,14 @@ module Bench
       @host, @port = [ host, port ]
     end
 
+    # TCP_NODELAY is set to disable buffering. In the case of Sanford
+    # communication, we have all the information we need to send up front and
+    # are closing the connection, so it doesn't need to buffer.
+    # See http://linux.die.net/man/7/tcp
+
     def call(name, params)
       socket = TCPSocket.open(@host, @port)
-      socket.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, true) # TODO - explain
+      socket.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, true)
       connection = Sanford::Protocol::Connection.new(socket)
       request = Sanford::Protocol::Request.new(name, params)
       connection.write(request.to_hash)
